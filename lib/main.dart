0e532432c07bb931d7a0c0dc9224e8747c7a085b
hp <1m2i3r4h@gmail.com>
2023-10-23 21:44:27 +0300

    create note_app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
