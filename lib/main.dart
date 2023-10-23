6abe4e7555118b90601db2fd14a627bccda12f08
hp <1m2i3r4h@gmail.com>
2023-10-23 22:11:50 +0300

    + theme to note_app + note_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/note_view.dart';
 
 void main() {
   runApp(NoteApp());
@@ -7,7 +8,12 @@ void main() {
 class NoteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: NoteView(),
+    );
   }
 }
-
