dffc18fbe822bf65e52129e70b82c2ab541b4530
hp <1m2i3r4h@gmail.com>
2023-10-26 12:04:06 +0300

    init Hive open Hive box to store data on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/views/constant/colors.dart';
 import 'package:notes_app/views/note_view.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNoteBox);
   runApp(NoteApp());
 }
 
